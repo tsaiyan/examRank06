5eb30b30403fd431b85bc680682e6f6e35a1207d
Artyom Baranov <medovar@MacBook-Pro-Artyom.local>
2022-09-03 14:19:06 +0300

    добавил комменты к select

--- mini_serv.cpp
+++ mini_serv.cpp
@@ -14,17 +14,17 @@ int nextUserID = 0;
 
 char readMessageBuffer[42*4096], writeMessageBuffer[42*4096];
 
-fd_set active, readyRead, readyWrite; // https://ru.manpages.org/fd_set/2
+fd_set activeFd, readyReadFd, readyWriteFd; // https://ru.manpages.org/fd_set/2
 
 void fatal_error() {
     write(2, "Fatal error\n", 12);
     exit(1);
 }
 
-// берет сообщение из буффера и отправляет всем пользователям через сокет, кроме отправителя
+// берет сообщение из буффера и отправляет всем пользователям через сокет, кроме того что в параметре
 void sendMessageToAllUsers(int currentUserSocket) {
     for (int i = 0; i <= maxUserSocket; i++)
-        if (FD_ISSET(i, &readyWrite) && i != currentUserSocket)
+        if (FD_ISSET(i, &readyWriteFd) && i != currentUserSocket)
             send(i, writeMessageBuffer, strlen(writeMessageBuffer), 0);
 }
 
@@ -43,7 +43,7 @@ int main(int argc, char **argv) {
     
     // зануление
     bzero(&usersID, sizeof(usersID));
-    FD_ZERO(&active);
+    FD_ZERO(&activeFd);
 
     // открытие сокета, команды гуглить или можно обойтись без них хардкодом
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
@@ -52,23 +52,32 @@ int main(int argc, char **argv) {
 
     maxUserSocket = serverSocket;
     // установить пользака активным
-    FD_SET(serverSocket, &active);
+    FD_SET(serverSocket, &activeFd);
 
     if ((bind(serverSocket, (const struct sockaddr *)&addr, sizeof(addr))) < 0)
         fatal_error();
     
-    // listen -
+    // listen - слушает соединение на сокете и ей можно задать максимальную очередь - макс число клиентов которое по очереди может быть обработано на сервере
     if (listen(serverSocket, 128) < 0)
         fatal_error();
 
     while (1) {
-        readyRead = readyWrite = active; // это фдшники или сокеты?
-        // select - проверка всех фдшников на готовность
-        if (select(maxUserSocket + 1, &readyRead, &readyWrite, NULL, NULL) < 0)
+        readyReadFd = readyWriteFd = activeFd; // это фдшники или сокеты?
+        // select - используется для эффективного слежения за несколькими файловыми дескрипторами — для ожидания, когда какой-то из них не станет «готов», то есть появится возможность чтения-записи данных, или с файловым дескриптором не возникнет «исключительная ситуация». https://ru.manpages.org/select/2
+        if (select(maxUserSocket + 1, &readyReadFd, &readyWriteFd, NULL, NULL) < 0)
             continue; // при первом запуске он выходит тут
+        
+        /*
+        1 аргумент select'a - nfds
+        Представляет собой целое число, на единицу большее максимального файлового дескриптора в любом из наборов. Другими словами, при добавлении файловых дескрипторов в наборы вам необходимо вычислять максимальное целое значение любого из них, а затем увеличивать это значение на единицу и передавать в nfds.
+        2 аргумент - readfds
+        Этот набор служит для слежения за появлением данных, доступных для чтения из любого файлового дескриптора. После возврата из select() в readfds остаются только те дескрипторы файлов, из которых возможно немедленное чтение.
+        3 аргумент - writefds
+        Этот набор служит для слежения за появлением места для записи данных в любой из файловых дескрипторов набора. После возврата из select() в writefds остаются только те файловые дескрипторы, в которые возможна немедленная запись.
+         */
 
         for (int socket = 0; socket <= maxUserSocket; socket++) {
-            if (FD_ISSET(socket, &readyRead) && socket == serverSocket) { // находит клиента
+            if (FD_ISSET(socket, &readyReadFd) && socket == serverSocket) { // находит клиента
                 int userSocket = accept(serverSocket, (struct sockaddr *)&addr, &addr_len);
                 if (userSocket < 0)
                     continue; // не нашел других клиентов
@@ -76,7 +85,7 @@ int main(int argc, char **argv) {
                 maxUserSocket = (userSocket > maxUserSocket) ? userSocket : maxUserSocket;
                 usersID[userSocket] = nextUserID; // записывает номер пользака
                 nextUserID += 1;
-                FD_SET(userSocket, &active);
+                FD_SET(userSocket, &activeFd);
 
                 sprintf(writeMessageBuffer, "server: client %d just arrived\n", usersID[userSocket]);
                 sendMessageToAllUsers(userSocket);
@@ -85,14 +94,14 @@ int main(int argc, char **argv) {
 
             // MARK: - чтение сообщения
             
-            if (FD_ISSET(socket, &readyRead) && socket != serverSocket) {
+            if (FD_ISSET(socket, &readyReadFd) && socket != serverSocket) {
                 // чтение данных из сокета
                 int readBytes = recv(socket, readMessageBuffer, 42*4096, 0);
                 // если ошибка, пользак отключился
                 if (readBytes <= 0) {
                     sprintf(writeMessageBuffer, "server: client %d just left\n", usersID[socket]);
                     sendMessageToAllUsers(socket);
-                    FD_CLR(socket, &active);
+                    FD_CLR(socket, &activeFd);
                     close(socket);
                     break;
                 }
